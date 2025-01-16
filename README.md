c2786a690509b0978633844a0037c64467764f9a
purple frog <theodorebaker08@gmail.com>
2025-01-16 13:35:59 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+build with `cargo build`
+and run with `qemu-arm ./target/thumbv7em-none-eabihf/debug/flora-os`
---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  rust-overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> {
+    overlays = [(import rust-overlay)];
+  };
+  toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+  libPkgs = with pkgs; [
+      toolchain
+      clippy
+      rust-analyzer
+      qemu
+    ];
+    libPath = pkgs.lib.makeLibraryPath libPkgs;
+in
+pkgs.mkShell {
+  packages = libPkgs;
+  LD_LIBRARY_PATH = libPath;
+}
